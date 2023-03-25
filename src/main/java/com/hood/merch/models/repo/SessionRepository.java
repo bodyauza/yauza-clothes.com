50d085a1a74080e7d7925353640063bedbf68a03
BogdanAndreev <62110663+bodyauza@users.noreply.github.com>
2023-03-25 14:07:06 +0300

    Добавлены файлы проекта.
    Проблемы: добавление одинаковых товаров в корзину, отправка html письма "post.html"

---
+++ src/main/java/com/hood/merch/models/repo/SessionRepository.java
@@ -0,0 +1,7 @@
+package com.hood.merch.models.repo;
+
+import jakarta.websocket.Session;
+
+public interface SessionRepository <S extends Session> {
+
+}
---
+++ src/test/java/com/hood/merch/MerchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hood.merch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
