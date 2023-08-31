8056319cba3321f0ccee70e6adcfd892df800c88
Bo20062020 <62110663+Bo20062020@users.noreply.github.com>
2023-08-31 23:47:25 +0300

    SpringBootTest

--- src/test/java/com/hood/merch/MerchApplicationTests.java
+++ src/test/java/com/hood/merch/MerchApplicationTests.java
@@ -3,7 +3,7 @@ package com.hood.merch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MerchApplication.class)
 class MerchApplicationTests {
 
 	@Test
