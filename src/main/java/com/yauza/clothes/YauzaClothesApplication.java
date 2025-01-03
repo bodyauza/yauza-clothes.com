f9ed8674d0cbf8278d2b1b782a510a5f996439b4
bodyauza <bogdanazino777@gmail.com>
2025-01-03 06:06:03 +0300

    Adding Dockerfile and docker-compose.yaml

--- src/main/java/com/yauza/clothes/YauzaClothesApplication.java
+++ src/main/java/com/yauza/clothes/YauzaClothesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MerchApplication {
+public class YauzaClothesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MerchApplication.class, args);
+        SpringApplication.run(YauzaClothesApplication.class, args);
 	}
 
 }
